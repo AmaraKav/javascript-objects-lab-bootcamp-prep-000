99dee6d4a94ad952795e63f8637e6e23990e571c
Kavindu Amarasinghe <amarasinghekavindu@yahoo.com>
2017-11-19 21:08:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ var recipes = {
 
 function updateObjectWithKeyAndValue(object, key, value){
   var old = key.value;
-  return Object.assign({}, obj, {[key]:value});
+  return Object.assign({}, object, {[key]:value});
 }
